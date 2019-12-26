80e3f278032fbad8224bdedbdb79625f1e2957ba
Danilov Igor <danilovigor773@gmail.com>
2019-12-26 18:42:38 +0500

    Added the more btn handler to add the ingredients for burger in builder-ctrl component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 0.8,
+    bacon: 0.3
+};
+
 class BurgerBuilder extends Component{
 
     state = {
@@ -11,14 +18,32 @@ class BurgerBuilder extends Component{
             meat: 2,
             cheese: 0,
             bacon: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientsHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const addedPrice = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = addedPrice + oldPrice;
+        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls addIngredients={this.addIngredientsHandler}/>
             </Aux>           
         );
     }
