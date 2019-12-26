5554f5886bfea649e4db4993e482c1e6f681c11c
Danilov Igor <danilovigor773@gmail.com>
2019-12-26 17:34:37 +0500

    Added buildCtrls component with the separate one inside.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from '../BuildControls/BuildControls.css';
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+
+const controls = [
+    {label: 'salad', type: 'salad'},
+    {label: 'cheese', type: 'cheese'},
+    {label: 'meat', type: 'meat'},
+    {label: 'bacon', type: 'bacon'}
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+         {controls.map(ctrl => (
+             <BuildControl key={ctrl.label} label={ctrl.label}/>
+         ))}
+    </div>
+);
+
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,29 @@ import classes from '../Burger/Burger.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+
+    let transFormedIngredinets = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            });
+            })
+            .reduce((arr, el) => (
+                arr.concat(el)
+            ), []);
+            
+        //console.log("transformed Ingr: ", transFormedIngredinets);
+        
+    if(transFormedIngredinets.length === 0){
+        transFormedIngredinets = <p>Please start adding ingridients!</p>
+    }
+        
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="cheese"/>
+            {
+                transFormedIngredinets
+            }
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
