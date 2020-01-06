7531d69cd6355c51010c81a4d678f45c43c71cc4
Danilov Igor <danilovigor773@gmail.com>
2020-01-06 15:07:11 +0500

    Added 'total price' to be shown within orderSummary cmp.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,6 +20,7 @@
              <ul>
                 {ingredientsList}
              </ul>
+             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
              <p>Continue to Checkout?</p>
              <Button btnType='Danger' clicked={props.purchaseCancel}>CANCEL</Button>
              <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
