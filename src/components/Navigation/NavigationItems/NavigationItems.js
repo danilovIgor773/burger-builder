a50df4e3b3587ea81c82475270e09234bb02b624
Danilov Igor <danilovigor773@gmail.com>
2020-01-06 17:54:24 +0500

    Added navigation multiple components without routing for now.

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from '../NavigationItems/NavigationItems.css';
+import NavigationItem from '../../Navigation/NavigationItems/NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem 
+            link='/'
+            active>
+        Burger Builder
+        </NavigationItem>
+        <NavigationItem 
+            link='/'>
+        Checkout
+        </NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
