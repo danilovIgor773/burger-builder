e16bde51c702c11cdbbdfa1a67144594036b4017
Danilov Igor <danilovigor773@gmail.com>
2020-01-07 17:09:59 +0500

    Added the some perfomace optimization when using Modal cmp.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,30 +2,31 @@
  import Aux from '../../../hoc/Aux';
  import Button from '../../UI/Button/Button';
 
- const orderSummary = (props) => {
 
-     const ingredientsSummary = props.ingredients;
-     //Here we form the list of ingredients that will be shown in a Modal and will be updated dynamically...
-     const ingredientsList = Object.keys(ingredientsSummary)
+ const orderSummary = (props) => {
+    const ingredientsSummary = props.ingredients;
+        //Here we form the list of ingredients that will be shown in a Modal and will be updated dynamically...
+    const ingredientsList = Object.keys(ingredientsSummary)
         .map((igKey) => {
             return <li key={igKey}>
                 <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
             </li>
-         });
-
-     return (
-         <Aux>
-             <h3>Your order</h3>
-             <p>A delicious burger with the following ingredients</p>
-             <ul>
+        });
+    
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients</p>
+            <ul>
                 {ingredientsList}
-             </ul>
-             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-             <p>Continue to Checkout?</p>
-             <Button btnType='Danger' clicked={props.purchaseCancel}>CANCEL</Button>
-             <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-         </Aux>
-     );
- };
+            </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType='Danger' clicked={props.purchaseCancel}>CANCEL</Button>
+            <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
+        </Aux>
+    ); 
 
+
+ }; 
  export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from '../Modal/Modal.css';
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }} 
-            className={classes.Modal}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component{
+    
+    //First steps to perfomace optimization...
+    //Modal cmp wraps the OrderSummary cmp in the BurgerBuilder container cmp. So we added shouldComponentUpdate to control Modal rendering...
+    //The Modal triggers by the click on ORDER NOW btn, so when using the build ctrls, there is no need to unnecessary render the Modal 
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+    
+    componentDidUpdate(){
+        console.log('[Modal] componentDidUpdate...');
+        
+    }
+
+    render(){        
+        return (
+        <Aux>
+            <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+            <div
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }} 
+                className={classes.Modal}>
+                {this.props.children}
+            </div>
+        </Aux>
+    );
+}
+}
+
+export default React.memo(Modal);
