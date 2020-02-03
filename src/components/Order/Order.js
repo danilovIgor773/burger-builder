64742558b00860b58f6f2c4c0390d430b2f2ccdf
Danilov Igor <danilovigor773@gmail.com>
2020-02-03 19:14:28 +0500

    Added orders fetch frfom firebase and their output.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,27 @@ import React from 'react';
 import classes from '../Order/Order.css';
 
 const order = (props) => {
-    return(
+    const ingredients = [];
+
+    for(let igName in props.ingredients){
+        ingredients.push({
+            name: igName,
+            amount: props.ingredients[igName]
+        })
+    }
+
+    let outputIngredients = ingredients.map(ingredient =>(
+        <span 
+            className={classes.Ingredients} key={ingredient.name}>
+                {ingredient.name} ({ingredient.amount})
+            </span>
+    ))
+
+
+    return( 
         <div className={classes.Order}>
-            <p>Ingredients: Salad(1)</p>
-            <p>Total price: <strong>USD 5.8</strong></p>
+            <p>Ingredients: {outputIngredients}</p>
+            <p>Total price: <strong>{props.price.toFixed(2)}</strong></p>
         </div>
     )
 }
